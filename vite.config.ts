cf2d01edbf7a32f5fa5a4da53203afaa4c4656c8
Stanislaw Cronberg <stanislawcronberg@gmail.com>
2024-10-26 16:02:40 +0200

    Initial code.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    base: '/',
+  };
+
+  if (command !== 'serve') {
+    config.base = '/blog-attempt-v2/'; // Change to your repo name
+  }
+
+  return config;
 });
